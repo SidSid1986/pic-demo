2d4c81b8a18247b6e63a31c407f32ce40d41b1ba
DESKTOP-V48IMQF\DELL <174677713@qq.com>
2025-08-06 17:27:51 +0800

    123

---
+++ src/views/Home/home.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Author: Sid Li
+ * @Date: 2025-08-06 17:13:36
+ * @LastEditors: Sid Li
+ * @LastEditTime: 2025-08-06 17:17:32
+ * @FilePath: \pic-demo\src\views\Home\home.vue
+ * @Description: 
+-->
+<template>
+  <div class="home-container">home</div>
+</template>
+
+<script setup>
+import {
+  getCurrentInstance,
+  onMounted,
+  onUnmounted,
+  ref,
+  watch,
+  computed,
+  reactive,
+  nextTick,
+} from "vue";
+
+onMounted(async () => {});
+
+onUnmounted(() => {});
+</script>
+
+<style lang="scss" scoped></style>
